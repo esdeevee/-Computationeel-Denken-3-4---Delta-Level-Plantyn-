2c275c07f39b7a533fa586c447137e5965e6410c
esdeevee <76687782+esdeevee@users.noreply.github.com>
2025-02-18 22:07:18 +0100

    Add files via upload

--- Extra 1/bisectie/evaluation/genereer 0.in en 0.out.py
+++ Extra 1/bisectie/evaluation/genereer 0.in en 0.out.py
@@ -50,31 +50,60 @@ file = open("0.out", "w")
 file.truncate()
 file.close()
 
+# wis alle gegevens in in.csv
+file = open("1.in", "w")
+file.truncate()
+file.close()
+# wis alle gegevens in out.csv
+file = open("1.out", "w")
+file.truncate()
+file.close()
+
+# wis alle gegevens in in.csv
+file = open("2.in", "w")
+file.truncate()
+file.close()
+# wis alle gegevens in out.csv
+file = open("2.out", "w")
+file.truncate()
+file.close()
+
+
 
 for i in range(50):
     with open('0.in', 'a') as file:
-        x = randint(10, 300)/100
-        n = randint(2, 20)
-        c = randint(10, 1000)/10
-        file.write('f(' + str(x) + ', ' + str(n) + ', ' + str(c) + ')')
+        a = round(randint(-200, 200)/10, 1)
+        b = round(randint(-200, 200)/10, 1)
+        file.write('>>> midden(' + str(a) + ', ' + str(b) + ')')
         file.write('\n')
 
-    with open('0.out', 'a') as file:
+        file.write(str(midden(a, b)))
+        file.write('\n')
+        #file.write('\n')
+
+for i in range(50):   
+    with open('1.in', 'a') as file:
+        x = round(randint(10, 300)/100, 2)
+        n = randint(2, 20)
+        c = round(randint(10, 1000)/10, 1)
+        file.write('>>> f(' + str(x) + ', ' + str(n) + ', ' + str(c) + ')')
+        file.write('\n')
+
         file.write(str(f(x, n, c)))
         file.write('\n')
         #file.write('\n')
-        
-    with open('0.in', 'a') as file:
-        n = randint(2, 20)
-        c = randint(200, 3000)/100
+
+for i in range(50):
+    with open('2.in', 'a') as file:
+        n = randint(2, 10)
+        c = round(randint(200, 30000)/100, 1)
         exponent = randint(-12, -6)
-        a = c**(1/n) - randint(1, 3)
-        b = c**(1/n) + randint(1, 3)
+        a = round(c**(1/n) - randint(1, 3), 1)
+        b = round(c**(1/n) + randint(1, 3), 1)
         e = 10**exponent
-        file.write('n_de_machtswortel_bisectie(' + str(n) + ', ' + str(c) + ', '+ str(a) + ', ' + str(b) + ', ' + str(e) + ')')
+        file.write('>>> n_de_machtswortel_bisectie(' + str(n) + ', ' + str(c) + ', '+ str(a) + ', ' + str(b) + ', ' + str(e) + ')')
         file.write('\n')
         
-    with open('0.out', 'a') as file:
         file.write(str(n_de_machtswortel_bisectie(n, c, a, b, e)))
         file.write('\n')
         #file.write('\n')
