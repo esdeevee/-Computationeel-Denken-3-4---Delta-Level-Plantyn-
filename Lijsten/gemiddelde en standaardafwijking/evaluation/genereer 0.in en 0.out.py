a4c4b2c99e8c3fa2a687798dc420e131befff81a
esdeevee <76687782+esdeevee@users.noreply.github.com>
2025-04-28 17:25:29 +0200

    Add files via upload

--- Lijsten/gemiddelde en standaardafwijking/evaluation/genereer 0.in en 0.out.py
+++ Lijsten/gemiddelde en standaardafwijking/evaluation/genereer 0.in en 0.out.py
@@ -1,18 +1,66 @@
+def gemiddelde(lijst):
+    som = 0
+    for getal in lijst:
+        som += getal
+    return round(som / len(lijst), 1)
+    
+def mediaan(lijst):
+    n = len(lijst)
+    lijst.sort()
+    if n % 2 == 1:
+        return round(lijst[int((n-1)/2)], 1)
+    else:
+        return round(gemiddelde([lijst[int(n/2 - 1)], lijst[int(n/2)]]), 1)
+
+def Q1(lijst):
+    n = len(lijst)
+    if n % 2 == 1:
+        return round(mediaan(lijst[:int((n+1)/2)]), 1)
+    else:
+        return round(mediaan(lijst[:int(n/2)]), 1)
+
+def Q3(lijst):
+    n = len(lijst)
+    if n % 2 == 1:
+        return round(mediaan(lijst[int((n-1)/2):]), 1)
+    else:
+        return round(mediaan(lijst[int(n/2):]), 1)
+
+def IKA(lijst):
+    return Q3(lijst) - Q1(lijst)
+
+def standaardafwijking(lijst):
+    from math import sqrt
+    n = len(lijst)
+    som = 0
+    gem = gemiddelde(lijst)
+    for getal in lijst:
+        som += (getal - gem)**2
+    return round(sqrt(som / (n-1)), 1)
+    
+"""
+lijst1 = [5, 3, 1, 4, 2]
+
+print(gemiddelde(lijst1))
+print(mediaan(lijst1))
+print(Q1(lijst1))
+print(Q3(lijst1))
+print(IKA(lijst1))
+print(standaardafwijking(lijst1))
+
+print('\n')
+
+lijst1 = [5, 3, 6, 1, 4, 2]
+
+print(gemiddelde(lijst1))
+print(mediaan(lijst1))
+print(Q1(lijst1))
+print(Q3(lijst1))
+print(IKA(lijst1))
+print(standaardafwijking(lijst1))
+"""
+
 from random import randint
-
-def eendjes_vissen(lijst, aantal):
-    maximum = 0
-    positie = 0
-    for i in range(len(lijst)):
-        som = 0
-        for j in range(aantal):
-            som += lijst[(i + j) % len(lijst)]
-        if som > maximum:
-            maximum = som
-            positie = i
-    return (positie, maximum)
-
-
 
 # wis alle gegevens in in.csv
 file = open("0.in", "w")
@@ -26,13 +74,18 @@ file.close()
 for i in range(1, 11):
     for k in range(10):  
         lijst = []
-        for j in range(5*i):
-            eendje = randint(1,10)
-            lijst.append(eendje)
-        aantal = randint(2, 5*i - 2)
+        aantal = randint(5, 10*i)
+        for j in range(aantal):
+            getal = randint(-10*i, 10*i)
+            lijst.append(getal)
+        """
         with open('0.in', 'a') as file:
-            file.write('>>> eendjes_vissen(' + str(lijst) + ', ' + str(aantal) + ')')
+            file.write('>>> gemiddelde(' + str(lijst) + ')')
             file.write('\n')
-            file.write(str(eendjes_vissen(lijst, aantal)))
+            file.write(str(gemiddelde(lijst)))
             file.write('\n')
-
+        """
+        print('>>> standaardafwijking(' + str(lijst) + ')')
+        #print('\n')
+        print(str(standaardafwijking(lijst)))
+        #print('\n')
